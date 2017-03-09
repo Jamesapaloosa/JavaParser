bae246460d4c24954254bc287db3edaa141ba5e9
James Gilders <jamesapaloosa1@gmail.com>
2017-03-09 11:18:33 -0700

    Create TESTER.java

---
+++ TESTER.java
@@ -0,0 +1,26 @@
+//This file is only for testing
+public class TESTER {
+	//TESTMETHOD
+	public int add(int a, int b){
+		int c = a + b;
+		return (c);
+	}
+
+	public float add(float a, float b){
+		float c = a + b;
+		return (c);
+	}
+	
+	public String add(String a, String b){
+		String c = a + b;
+		return c;
+	}
+	
+	public int Bouncer(int a){
+		return(a);
+	}
+	
+	public int Get2(){
+		return 2;
+	}
+}
