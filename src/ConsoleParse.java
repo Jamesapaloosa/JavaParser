a895c3043e45cfe6546e66149c6dcfb82705e7bd
CarlinLiu <CarlinLiu@users.noreply.github.com>
2017-03-15 12:02:38 -0600

    Updated Console Parse function

--- src/ConsoleParse.java
+++ src/ConsoleParse.java
@@ -125,31 +125,32 @@ public class ConsoleParse
 	}
 	
 	
+
 	public boolean ParseProgramInput(String[] input, boolean Verbose)
 	{
 		Interface MainI = new Interface();
 		//System.out.println("Console Parser");
 		int i1 = 1;
 		int i2 = 1;
-		int j1 = input[1].length();
-		int j2 = input[1].length();
+		int j1 = input[0].length();
+		int j2 = input[0].length();
 		boolean noError = true;	
 		boolean runH = false;
 		boolean runV = false;
 				
-		if(input[1].charAt(0) == '-')
+		if(input[0].charAt(0) == '-')
 		{
-			//System.out.println("1");
-			if(input[1].charAt(1) == '-')
+			System.out.println("1");
+			if(input[0].charAt(1) == '-')
 			{
 				//System.out.println("2");
-				if(input[1].compareToIgnoreCase("--help") == 0)
+				if(input[0].compareToIgnoreCase("--help") == 0)
 				{
-					//System.out.println("2.1");
-					MainI.PrintMenu();
-					//break;
+					System.out.println("Qualifier --help (-h, -?) should not appear with any command-line arguments.");
+					MainI.PrintSyn2();
+					System.exit(0);
 				}
-				else if(input[1].compareToIgnoreCase("--verbose") == 0)
+				else if(input[0].compareToIgnoreCase("--verbose") == 0)
 				{
 					//System.out.println("2.2");
 					if(Verbose == false)
@@ -166,7 +167,7 @@ public class ConsoleParse
 				}
 				else
 				{
-					System.out.println("Unrecognized " + input[1]);
+					System.out.println("Unrecognized " + input[0]);
 					MainI.PrintSyn2();
 				}
 			}
@@ -177,19 +178,23 @@ public class ConsoleParse
 				{
 					//use AND and print out char 0
 					//System.out.println("index "+i1);
-					char x = input[1].charAt(i1);
+					char x = input[0].charAt(i1);
 					
 					if(x == 'h')
 					{
 						//System.out.println("h");
-						runH = true;
+						//runH = true;
 						//continue;
+						System.out.println("Qualifier --help (-h, -?) should not appear with any command-line arguments.");
+						System.exit(0);
 					}
 					else if(x == '?') 
 					{
 						//System.out.println("?");
-						runH = true;
+						//runH = true;
 						//continue;
+						System.out.println("Qualifier --help (-h, -?) should not appear with any command-line arguments.");
+						System.exit(0);
 					}
 					else if(x == 'v')
 					{
@@ -200,7 +205,7 @@ public class ConsoleParse
 					else 
 					{
 						noError = false;
-						System.out.println("Unrecognized qualifier '" + input[1].charAt(i1) + "' in '"+ input[1] +"' ");
+						System.out.println("Unrecognized qualifier '" + input[0].charAt(i1) + "' in '"+ input[0] +"' ");
 						MainI.PrintSyn2();
 						break;
 					}
@@ -211,11 +216,6 @@ public class ConsoleParse
 			}
 			if(noError == true)
 			{
-				if(runH == true)
-				{
-					//System.out.println("running help");
-					MainI.PrintSyn();
-				}	
 				if(runV == true)
 				{
 					//System.out.println("running verbose");
