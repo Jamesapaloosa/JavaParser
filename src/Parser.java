2e88cd9da438465ea50ea71c440e20367ebaf066
James Gilders <jamesapaloosa1@gmail.com>
2017-03-16 13:43:00 -0600

    Update Parser.java

--- src/Parser.java
+++ src/Parser.java
@@ -103,11 +103,7 @@ public class Parser {
 	//Simple method that can print all the fields in a method
 	public void PrintFields(){
 		for(Field field : fields){
-			try{
 			System.out.println("Field = " + field.getName());
-			}catch(NullPointerException e){
-				System.out.println("Null was printed");
-			}
 		}
 	}
 //============================================================================
